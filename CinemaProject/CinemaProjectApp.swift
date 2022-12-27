d32d201537bb39cb29ffa88c05051f792b9a119d
Dostonjon-98 <mahmanazarovdoston98@gmail.com>
2022-12-27 15:47:34 +0500

    Initial Commit

---
+++ CinemaProject/CinemaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaProjectApp.swift
+//  CinemaProject
+//
+//  Created by MacBook on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CinemaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
